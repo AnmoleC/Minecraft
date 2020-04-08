cedb85f19957a98648a9ce3f98e517315a14ec27
AnmoleC <anmole_chaudry@hotmail.com>
2020-04-07 22:05:52 -0400

    Initial Commit

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+echo "Synchronizing Minecraft World"
+git pull
+echo "Starting Minecraft server"
+java -Xms4096M -Xmx8192M -jar server.jar nogui 
+#set /p input="To close server enter q"
+echo "Persisting Minecraft world to GitHub"
+git add .
+git commit -m "Updating Minecraft world"
+git push
